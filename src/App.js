fd4a844a0586fb72fdfffb26be664e372e85cb30
Maria Ilieva <123466748+marianilieva@users.noreply.github.com>
2023-04-16 11:25:44 +0300

    Forth update with login

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Geographical from './components/AllDependencies/Geographical/Geographical
 import Social from './components/AllDependencies/Social/Social';
 import Interests from './components/AllDependencies/Interests/Interests';
 import Error from './components/AllDependencies/Error/Error';
+import Login from './components/UserInteractions/Login/Login';
 import Register from './components/UserInteractions/Register/Register';
 
 import Footer from './components/Footer/Footer';
@@ -37,6 +38,7 @@ function App() {
           <Route path='/social' element={<Social />} />
           <Route path='/interests' element={<Interests />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
           <Route path='*' element={<Error />} />
         </Routes>
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,15 @@ import styles from './Footer.module.css';
 
 export default function Footer () {
     return (
-        <>
+        <div className={styles['footerContainer']}>
             <div className={styles['footerMenu']}>
                 <Link to="/register">Register</Link>
+                <Link to="/login">Login</Link>
             </div>
             <div className={styles['footer']}>
                 Copyrights &copy; of the app and all of its features (including 
                 text) belong to Maria Nikitova Ilieva, 03.04.2023
             </div>
-        </>
+        </div>
     );
 }

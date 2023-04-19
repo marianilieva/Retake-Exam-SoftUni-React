1bc640e24d93967c273d67dc39f648a720e02d74
Maria Ilieva <123466748+marianilieva@users.noreply.github.com>
2023-04-19 08:45:07 +0300

    Update with started commenting

--- src/components/AllDependencies/Interactions/Statistics/Statistics.js
+++ src/components/AllDependencies/Interactions/Statistics/Statistics.js
@@ -1,11 +1,11 @@
 import { useContext } from 'react';
 import styles from './Statistics.module.css';
 
-export default function Statistics () {
+export default function Statistics ({comments}) {
     
     return (
         <div className={styles['statistics']}>
-            
+            { comments }
         </div>
     );
 }
